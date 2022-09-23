72086d3b3de955ab5341205e35b12bd90e721853
Mateus Zucato <mateuszucato@hotmail.com>
2022-09-23 17:33:01 -0300

    update code

---
+++ src/Api.js
@@ -0,0 +1,49 @@
+const API_KEY = "96ff0fcdcfdad139b940aa2ba8fa0f7b";
+
+const categories = [
+		{
+				name: "trending",
+				title: "Em Alta",
+				path: '/trending/all/week?api_key=${API_KEY}&language=pt-BR'
+		},
+		{
+				name: "netflixOriginals",
+				title: "Originais Netflix",
+				path: '/dicovery/tv?api_key=${API_KEY}&with_networks=213'
+		},
+		{
+				name: "topRated",
+				title: "Populares",
+				path: '/movie/top_rated?api_key=${API_KEY}&language=pt-BR'
+		},
+		{
+				name: "comedy",
+				title: "Comedias",
+				path: '/dicovery/tv?api_key=${API_KEY}&with_genres=35'
+		},
+		{
+				name: "romance",
+				title: "Romances",
+				path: '/dicovery/tv?api_key=${API_KEY}&with_genres=10749'
+		},
+		{
+				name: "documentary",
+				title: "Documentários",
+				path: '/dicovery/tv?api_key=${API_KEY}&with_genres=99'
+		},
+		{
+				name: "horror",
+				title: "Terror",
+				path: '/dicovery/tv?api_key=${API_KEY}&with_genres=27'
+		},
+];
+
+export const getMovies = async (path) => {
+    try {
+    let url = 'https://api.themoviedb.org/3${patch}';
+    const response = await fetch(url);
+    return await responde.json();
+  } catch (error) {
+    console.log("error getMovies: ", error);
+  };
+}
