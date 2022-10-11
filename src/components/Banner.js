1bd03223bf44a290cc36276872ab23912f3f4412
Mateus Zucato <mateuszucato@hotmail.com>
2022-10-11 05:34:41 -0300

    banner.css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,17 +2,19 @@ import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
 import React, { useEffect } from 'react';
 import { getMovies } from '../Api';
 import "./Banner.css";
+import categories from "../Api";
 
 export default function Banner() {
-    const [movie, setMovies] = React.useState ({});
-    const fecthRandomMovie = async (_path) => {
+    const [movie, setMovie] = React.useState ({});
+
+    const fecthRandomMovie = async () => {
         try {
             const netflixOriginalsCategory = categories.find(
-                (category) => category.name == " netflixOriginals"
+                (category) => category.name == "netflixOriginals"
             );
             const data = await getMovies (netflixOriginalsCategory.path);
             const movies = data?.results;
-            const randomIndex = Math.floor(Math.random() * data.results.length);
+            const randomIndex = Math.floor(Math.random() * movies.length);
             setMovie(movies(randomIndex))
         }   catch (error) {
             console.log("Banner fetchRandomMovie error: ", error);
@@ -20,8 +22,13 @@ export default function Banner() {
     };
 
     useEffect (() => {
-        
+        fecthRandomMovie();
     }, []);
   
-    return <div>Banner</div>
+    return <header className="banner-container" syle={{
+        backgroundSize: "cover",
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        roundPosition: "center-center"
+    }}
+    />
 }
